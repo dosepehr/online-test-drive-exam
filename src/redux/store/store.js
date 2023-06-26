3a9b01af582eded7294ab6f1b8f91e44252d20c1
dosepehr <dosepehr@gmail.com>
2023-06-26 21:08:48 +0330

    add apiSlice

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import examSlice from '../reducers/examSlice';
+import { apiSlice } from '../reducers/apiSlice';
 
 const store = configureStore({
     reducer: {
         exam: examSlice,
+        [apiSlice.reducerPath]: apiSlice.reducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 export default store;
